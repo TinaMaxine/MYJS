706d1e9bc476bb1c7819b553efaccf068b711b91
Anjani <anjanipriyanka01@gmail.com>
2023-01-28 13:25:05 +0530

    wayToCreateObject

--- PracticeAJAX/script.js
+++ PracticeAJAX/script.js
@@ -16,6 +16,8 @@ function getDetails(){
     for (i=0;i<=users.length;i++){
         if(username==users[i].username && password==users[i].password){
             console.log("Login Success!");
+            window.location.href="https://www.w3schools.com"; 
+            // window.location.replace("https://www.w3schools.com");
         }
         else{
             console.log("Authentication error");
