218e5a720854dd457d8c5822f921784ed174b213
Anjani <anjanipriyanka01@gmail.com>
2023-02-21 03:27:21 +0530

    videoPlayer page Added with help

--- VideoStreamingApp/app.js
+++ VideoStreamingApp/app.js
@@ -29,26 +29,84 @@ ball.addEventListener("click",()=>{
     })
     ball.classList.toggle("active");
 })
-// var arrImgURL=[]
-// var random=Math.floor((Math.random()*100)+8);
-// console.log(random);
-// while(arrImgURL.length<=32){
-//    // fetch the JSON data from the API
-//     fetch('https://tinamaxine.github.io/index.json')
-//     .then(response => response.json())
-//     .then(data => {
-//   // loop through the data array to find the object with the matching ID
-//   const id = random; // example ID
-//   const object = data.data.find(obj => obj._id === id);
-//   if (object) {
-//       // get the image URL from the object
-//       const imageUrl = object.imageUrl;
-//       arrImgURL.push(imageUrl)
-//     console.log(imageUrl); // output the URL to the console
-//   } else {
-//       arrImgURL.push(imageUrl)
-//       console.log('Object with ID ' + id + ' not found in data.');
-//   }
-// })
-// .catch(error => console.error(error));
-// }
+
+function generateRandomNumbers(min, max) {
+    const numCount = 32;
+    const resultImgURL = [];
+    
+    while (resultImgURL.length < numCount) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      
+      if (!resultImgURL.includes(randomNumber)) {
+        resultImgURL.push(randomNumber);
+      }
+    }
+    
+    return resultImgURL;
+  }
+  
+  const randomNumbers = generateRandomNumbers(1, 32);
+//   console.log(randomNumbers);
+
+
+  fetchImgURLs(randomNumbers);
+ 
+  var  arrImgURL=[];
+  var  arrImgTitle=[];
+  function fetchImgURLs(r){
+    const thumbnails=document.querySelectorAll(".movie-list-item-image");
+    r.forEach(element => {
+        fetch('https://tinamaxine.github.io/index.json')
+       .then(response => response.json())
+       .then(data => {
+        const id=element;
+        const object = data.data.find(obj => obj._id === id);
+       
+        if (object) {
+           // get the image URL from the object
+           const imageUrl = object.imageUrl;
+           const Title=object.name
+           arrImgTitle.push(Title)
+           arrImgURL.push(imageUrl);
+        //    console.log(object,imageUrl);
+            popURLs(arrImgURL,thumbnails);
+            TextChange()
+        }
+      
+        })
+    })
+  }
+//   console.log((`arrayyyy${arrImgURL}`));
+//   console.log("hello");
+
+
+// console.log(thumbnails);
+
+function popURLs(arr,thumbs){
+    arr.forEach((url,index)=>{
+        const img=thumbs[index];
+        img.setAttribute('src',url);
+        // console.log(url);
+        // console.log(thumbs[index]);
+    });
+}
+var movieTitle=document.querySelectorAll(".movie-list-item-title")
+function TextChange(){
+    console.log("textchange called");
+    movieTitle.forEach((element,index)=>{
+      element.innerHTML=arrImgTitle[index]
+    });
+}
+
+// const API_KEY = 'AIzaSyB9ycVXkFCOWqO5SgAGHP6dnmMGdoEoHmE';
+// const searchQuery = 'pixar movie trailer'; // Replace with your own search query
+
+// fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyB9ycVXkFCOWqO5SgAGHP6dnmMGdoEoHmE&type=video&part=snippet&maxResults=100&q='pixar movie trailer'`)
+//   .then(response => response.json())
+//   .then(data => {
+//     const videos = data.items;
+//     // Extract video links from the videos array
+//     const videoLinks = videos.map(video => `https://www.youtube.com/watch?v=${video.id.videoId}`);
+//     console.log(videoLinks);
+//   })
+//   .catch(error => console.error(error));
